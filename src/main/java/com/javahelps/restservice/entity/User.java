fef33439009337f237084d566df133b0086e4618
aykut16 <50962873+aykut16@users.noreply.github.com>
2020-11-18 01:36:03 +0200

    c

--- src/main/java/com/javahelps/restservice/entity/User.java
+++ src/main/java/com/javahelps/restservice/entity/User.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 import org.hibernate.annotations.Proxy;
 
@@ -23,6 +24,7 @@ public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.TABLE, generator = "product_names")
+	@TableGenerator(name="product_names", table="id", schema="q8ucpgcjotv3c0vp")
 	private int id;
 	
 	private String barcode;
