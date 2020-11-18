03fe258bd456ba95671aa01cd59a22992c1f7f35
aykut16 <50962873+aykut16@users.noreply.github.com>
2020-11-19 00:11:05 +0200

    v

--- src/main/java/com/javahelps/restservice/entity/User.java
+++ src/main/java/com/javahelps/restservice/entity/User.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -19,7 +20,7 @@ public  class User {
 	
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	@Column(name = "barcode")
